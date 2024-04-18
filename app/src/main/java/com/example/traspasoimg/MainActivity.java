806a92b7c29a549eb7570e63fb95fbfe90f0267c
Mauricio <stb539@gmail.com>
2024-04-18 11:19:59 -0400

    Initial commit

---
+++ app/src/main/java/com/example/traspasoimg/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.traspasoimg;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView imagen1;
+    ImageView imagen2;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        imagen1=(ImageView) findViewById(R.id.imageView);
+        imagen2=(ImageView) findViewById(R.id.imageView2);
+
+        imagen1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                llamar(""+R.drawable.ic_launcher_foreground,""+R.color.white);
+            }
+        });
+
+        imagen2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                llamar(""+R.drawable.ojo,""+R.color.black);
+            }
+        });
+    }
+
+    // metodo afuera del onCreate
+    protected void llamar(String image, String color){
+        Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+        intent.putExtra("image", image);
+        intent.putExtra("color", color);
+        startActivity(intent);
+
+    }
+}
